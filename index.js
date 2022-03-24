497b5eb77c61f122c833e14b489fcca0be8899f2
Omar <ojhb1@gcloud.ua.es>
2022-03-24 23:47:11 +0100

    Primer API REST con Express parametrizable(Guia 6)

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+'use strict'
 var http = require('http');
+const port = process.env.PORT || 8888;
+const express = require('express');
+const app = express();
 
 http.createServer( (request, response) => {
 response.writeHead(200, {'Content-Type': 'text/plain'});
 response.end('Hola a todas y a todos!\n');
 }).listen(8080);
-console.log('Servidor ejecutándose en puerto 8080...')
+
+app.get('/hola/:unNombre', (req, res) => {
+    res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` });
+    });
+app.listen(port, () => {
+    console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
+    });
